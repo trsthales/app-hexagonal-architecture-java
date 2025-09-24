48562e92c2f29df7f19737da1ebb6d56b5c2435f
thales87 <thales87@gmail.com>
2025-09-24 03:32:56 -0300

    corrigindo launcher

--- bootstrap/src/main/java/com/thales/shop/bootstrap/Launcher.java
+++ bootstrap/src/main/java/com/thales/shop/bootstrap/Launcher.java
@@ -3,19 +3,25 @@ package com.thales.shop.bootstrap;
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
 
 public class Launcher {
+    private static final int PORT = 8080;
+
     private UndertowJaxrsServer server;
 
     public static void main(String[] args) {
-        new Launcher().startOnDefaultPort();
+        new Launcher().startOnPort(PORT);
     }
 
-    public void startOnDefaultPort() {
-        server = new UndertowJaxrsServer();
+    public void startOnPort(int port) {
+        server = new UndertowJaxrsServer().setPort(port);
         startServer();
     }
 
     private void startServer() {
         server.start();
         server.deploy(RestEasyUndertowShopApplication.class);
+    }
+
+    public void stop() {
+        server.stop();
     }
 }
